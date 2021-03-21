36a40a50d8d5047c4ecaf261e1a151255682139b
Arun <arun8778451315@gmail.com>
2021-03-21 23:40:11 +0530

    CricInfo Player Stats Array Added

--- Web_Scrapping_08_03_2021/raw/poc/cricinfo/leaderboard.js
+++ Web_Scrapping_08_03_2021/raw/poc/cricinfo/leaderboard.js
@@ -1,6 +1,10 @@
 let request = require("request");
 let cheerio = require("cheerio");
-let extractor = require("./singleMatchDetails");
+// let extractor = require("./singleMatchDetails");
+
+let statsArr = [];
+let globalCount = 0;
+let localCount = 1;
 
 let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results";
 
@@ -9,12 +13,86 @@ request(url,cb);
 function cb(err, response, html) {
     let chSelector = cheerio.load(html);
     let matches = chSelector(".col-md-8.col-16");
-    console.log(matches.length);
+    globalCount = matches.length;
     for (let i = 0;i < matches.length;i++) {
         let allAnchorOfMatch = chSelector(matches[i]).find(".match-cta-container .btn.btn-sm.btn-outline-dark.match-cta");
         let url = chSelector(allAnchorOfMatch[2]).attr("href");
-        let fullUrl = "https://www.espncricinfo.com" + link;
+        let fullUrl = "https://www.espncricinfo.com" + url;
         // console.log(fullUrl);
-        extractor.extract(fullUrl);
+        // extractor.extract(fullUrl);
+        request(fullUrl,cbDetails);
+    }
+}
+
+function cbDetails(err, response, html) {
+    let chSelector = cheerio.load(html);
+    //Find winningTeam
+    let teams = chSelector(".match-info.match-info-MATCH .teams").find(".team");  
+    let winnerTeamNum = 0;   //Winning Team index num
+    let winnerName = "";
+    for (let i = 0;i < teams.length;i++) {
+        if (chSelector(teams[i]).hasClass("team-gray") == false) {
+            winnerTeamNum = i;
+            winnerName = chSelector(teams[i]).find("p").text();
+        }
+    }
+
+    let batsmenBox = chSelector(".table.batsman");   
+    let winnerBatsmenBox = chSelector(batsmenBox[winnerTeamNum]);  //Winning team batsmen box as a whole
+
+    let winnerBatsmenBoxHeading = chSelector(winnerBatsmenBox).find("thead").find("th");   //Headings
+    let winnerBatsmenBoxBody = chSelector(winnerBatsmenBox).find("tbody").find("tr");     //Values corresponding to headings 
+
+    winTeamDetails(chSelector, winnerBatsmenBoxBody, winnerBatsmenBoxHeading);
+    
+    if (localCount == globalCount) {
+        console.table(statsArr);
+    }
+    else {
+        localCount++;
+    }
+}
+
+function winTeamDetails(chSelector, winnerBatsmenBoxBody, winnerBatsmenBoxHeading) {
+    //Map according to the heading and values and add individual batsmen details. Returns an array of objects with batsmen details.
+    for (let i = 0;i < winnerBatsmenBoxBody.length-1;i+=2) {    
+        let eachBatsmen = chSelector(winnerBatsmenBoxBody[i]).find("td");
+        // let batsmenDetails = {};
+        let name = "";
+        let runs = 0;
+        for (let j = 0;j < eachBatsmen.length;j++) {
+            if (j == 0) {
+                name = chSelector(eachBatsmen[j]).find("a").text();
+                // batsmenDetails[chSelector(winnerBatsmenBoxHeading[j]).text()] = name;
+            }
+            else if (j == 2) {
+                runs = Number(chSelector(eachBatsmen[j]).text());
+            }
+            // else if (j != 1 && j != 4) {
+            //     batsmenDetails[chSelector(winnerBatsmenBoxHeading[j]).text()] = chSelector(eachBatsmen[j]).text();
+            // }
+        }
+        addToLeaderboard(name, runs);
+        // statsArr.push(batsmenDetails);   //Finally added all batsmen details of the team to the array.
+    }
+    // console.log(winnerName);
+    // console.table(statsArr);   //Takes array of objects as input. Prints content in a tabular (table) format.
+}
+
+
+function addToLeaderboard(playerName, playerRuns) {
+    let playerExists = false;
+    for (let i = 0;i < statsArr.length;i++) {
+        if (statsArr[i].name == playerName) {
+            statsArr[i].runs += playerRuns;
+            playerExists = true;
+            break;
+        }
+    }
+    if (playerExists == false) {
+        statsArr.push({
+            name: playerName,
+            runs: playerRuns
+        });
     }
 }
--- Web_Scrapping_08_03_2021/raw/poc/cricinfo/singleMatchDetails.js
+++ Web_Scrapping_08_03_2021/raw/poc/cricinfo/singleMatchDetails.js
@@ -1,5 +1,6 @@
 let request = require("request");
 let cheerio = require("cheerio");
+let statsArr = [];
 
 // let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard";
 
@@ -8,7 +9,6 @@ function singlePageExtractor(url) {
 }
 
 function cb(err, response, html) {
-
     let chSelector = cheerio.load(html);
     //Find winningTeam
     let teams = chSelector(".match-info.match-info-MATCH .teams").find(".team");  
@@ -27,30 +27,48 @@ function cb(err, response, html) {
     let winnerBatsmenBoxHeading = chSelector(winnerBatsmenBox).find("thead").find("th");   //Headings
     let winnerBatsmenBoxBody = chSelector(winnerBatsmenBox).find("tbody").find("tr");     //Values corresponding to headings 
 
-    let teamBatsmenDetails =  winningTeamBatsmenDetails(winnerBatsmenBoxHeading, winnerBatsmenBoxBody, chSelector);
-    console.log(winnerName);
-    console.table(teamBatsmenDetails);   //Takes array of objects as input. Prints content in a tabular (table) format.
-}
-
-//Map according to the heading and values and add individual batsmen details. Returns an array of objects with batsmen details.
-function winningTeamBatsmenDetails(winnerBatsmenBoxHeading, winnerBatsmenBoxBody, chSelector) {
-    let teamBatsmenDetails = [];
+    //Map according to the heading and values and add individual batsmen details. Returns an array of objects with batsmen details.
     for (let i = 0;i < winnerBatsmenBoxBody.length-1;i+=2) {    
         let eachBatsmen = chSelector(winnerBatsmenBoxBody[i]).find("td");
-        let batsmenDetails = {};
+        // let batsmenDetails = {};
+        let name = "";
+        let runs = 0;
         for (let j = 0;j < eachBatsmen.length;j++) {
             if (j == 0) {
-                let batsmenName = chSelector(eachBatsmen[j]).find("a").text();
-                batsmenDetails[chSelector(winnerBatsmenBoxHeading[j]).text()] = batsmenName;
+                name = chSelector(eachBatsmen[j]).find("a").text();
+                // batsmenDetails[chSelector(winnerBatsmenBoxHeading[j]).text()] = name;
             }
-            else if (j != 1 && j != 4) {
-                batsmenDetails[chSelector(winnerBatsmenBoxHeading[j]).text()] = chSelector(eachBatsmen[j]).text();
+            else if (j == 2) {
+                runs = Number(chSelector(eachBatsmen[j]).text());
             }
+            // else if (j != 1 && j != 4) {
+            //     batsmenDetails[chSelector(winnerBatsmenBoxHeading[j]).text()] = chSelector(eachBatsmen[j]).text();
+            // }
         }
-        teamBatsmenDetails.push(batsmenDetails);   //Finally added all batsmen details of the team to the array.
+        addToLeaderboard(name, runs);
+        // statsArr.push(batsmenDetails);   //Finally added all batsmen details of the team to the array.
     }
+    // console.table(statsArr);
+    // console.log(winnerName);
+    // console.table(statsArr);   //Takes array of objects as input. Prints content in a tabular (table) format.
+}
 
-    return teamBatsmenDetails;
+
+function addToLeaderboard(playerName, playerRuns) {
+    let playerExists = false;
+    for (let i = 0;i < statsArr.length;i++) {
+        if (statsArr[i].name == playerName) {
+            statsArr[i].runs += playerRuns;
+            playerExists = true;
+            break;
+        }
+    }
+    if (playerExists == false) {
+        statsArr.push({
+            name: playerName,
+            runs: playerRuns
+        });
+    }
 }
 
 module.exports = {
