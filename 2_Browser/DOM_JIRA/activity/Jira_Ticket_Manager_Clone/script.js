8755960daf4473b9a65dfc54e4f31a42cf616b35
Arun <arun8778451315@gmail.com>
2021-05-06 02:49:36 +0530

    Todo responsiveness complete (Jira)

--- 2_Browser/DOM_JIRA/activity/Jira_Ticket_Manager_Clone/script.js
+++ 2_Browser/DOM_JIRA/activity/Jira_Ticket_Manager_Clone/script.js
@@ -4,41 +4,52 @@ let modalContainer = document.querySelector(".modal-container");
 let addBtn = document.querySelector(".add");
 let priorityColor = document.querySelectorAll(".priority-color");
 let colors = ["lightpink", "lightblue", "lightgreen", "black"];
-let cColor = colors[colors.length-1];
+let cColor = colors[colors.length - 1];
 let taskArea = document.querySelector(".task-area");
 let collectiveTicketContainer = document.querySelector(".collective-ticket-container");
-let flag = false;
+let removeBtn = document.querySelector(".remove");
+let addFlag = false;
+let removeFlag = false;
+let lockClass = "fa-lock";
+let unlockClass = "fa-lock-open";
+let ticketsArr = [];
 
-for (let i = 0;i < colorElemArr.length;i++) {
-    colorElemArr[i].addEventListener("click", function() {
-        console.log('clicked');
-        let childElemsArr = colorElemArr[i].children;
-        let colorClassArr = childElemsArr[0].classList;
-        mainContainerBox.style.backgroundColor = colorClassArr[0];
-    });
+if (localStorage.getItem("availableTickets")) {
+    let strTicketsArr = localStorage.getItem("availableTickets");
+    ticketsArr = JSON.parse(strTicketsArr);
+    for (let i = 0; i < ticketsArr.length; i++) {
+        let ticketObj = ticketsArr[i];
+        crudTicketUtility(ticketObj);
+    }
 }
 
-addBtn.addEventListener("click", function() {
-    if (flag == false) {
-        setToDefault();
+addBtn.addEventListener("click", function () {
+    if (addFlag == false) {
         modalContainer.style.display = "flex";
     }
-    flag = !flag;
+    else {
+        setToDefault();
+    }
+    addFlag = !addFlag;
 });
 
-taskArea.addEventListener("keydown", function(e) {
+removeBtn.addEventListener("click", function () {
+    removeFlag = !removeFlag;  //Switch between delete action and non-delete
+});
+
+taskArea.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
         let task = taskArea.value;
         createTicket(task, cColor);
         setToDefault();
-        flag = false;        
+        addFlag = false;
         // console.log("task", task, "color", cColor);    
     }
-})
+});
 
-for (let i = 0;i < priorityColor.length;i++) {
-    priorityColor[i].addEventListener("click", function() {
-        priorityColor.forEach(function(colorElem) {
+for (let i = 0; i < priorityColor.length; i++) {
+    priorityColor[i].addEventListener("click", function () {
+        priorityColor.forEach(function (colorElem) {
             colorElem.classList.remove("border");
         });
         priorityColor[i].classList.add("border");
@@ -46,93 +57,141 @@ for (let i = 0;i < priorityColor.length;i++) {
     });
 }
 
+for (let i = 0; i < colorElemArr.length; i++) {  //Filtering tickets according to toolbar priority actions
+    colorElemArr[i].addEventListener("click", function () {
+
+        let childElemsArr = colorElemArr[i].children;
+        let colorClassArr = childElemsArr[0].classList;
+        let filterColor = colorClassArr[0];
+        // mainContainerBox.style.backgroundColor = filterColor;
+
+        let filteredTicketsArr = ticketsArr.filter(function (ticketObj, idx) {
+            if (filterColor === "black") return true;
+            return ticketObj.color === filterColor;
+        });
+
+        let crudTicketsArr = collectiveTicketContainer.children;
+        for (let i = collectiveTicketContainer.children.length - 1; i >= 0; i--) {
+            collectiveTicketContainer.removeChild(crudTicketsArr[i]);
+        }
+        filteredTicketsArr.forEach(function (ticketObj) {
+            crudTicketUtility(ticketObj);
+        });
+
+    });
+}
+
+function crudTicketUtility(ticketObj) {
+    let ticket = document.createElement("div");
+    ticket.setAttribute("class", "ticket-container");
+    ticket.innerHTML = `
+                        <div class="head-color ${ticketObj.color}"></div>
+                        <div class="ticket-sub-container">
+                            <h2 class="task-id">#${ticketObj.id}</h2>
+                            <p class="task-desc" contenteditable="false">${ticketObj.task}</p>
+                        </div>
+                        <div class="lock-container">
+                            <i class="fas fa-lock"></i>
+                        </div>
+                        `;
+    collectiveTicketContainer.appendChild(ticket);
+
+    let ticketCrudSpace = ticket.querySelector(".ticket-sub-container");
+    let ticketLock = ticket.querySelector(".fas");
+    handleTicketCrud(collectiveTicketContainer, ticket, ticketCrudSpace, ticketObj.id);
+    handleTicketLock(ticket, ticketLock);
+}
+
 function createTicket(task, color) {
+    let id = shortid();
     let ticket = document.createElement("div");
     ticket.setAttribute("class", "ticket-container");
     ticket.innerHTML = `
                         <div class="head-color ${color}"></div>
                         <div class="ticket-sub-container">
-                            <h2 class="task-id">#sampleId</h2>
-                            <p class="task-desc" contenteditable="true">${task}</p>
+                            <h2 class="task-id">#${id}</h2>
+                            <p class="task-desc" contenteditable="false">${task}</p>
+                        </div>
+                        <div class="lock-container">
+                            <i class="fas fa-lock"></i>
                         </div>
                         `;
+    ticketsArr.push({ task, color, id });
+    let strTicketsArr = JSON.stringify(ticketsArr);
+    localStorage.setItem("availableTickets", strTicketsArr);
+
     collectiveTicketContainer.appendChild(ticket);
-    let tracker = 0;
-    
-    ticket.addEventListener("click", function() {
-        tracker = tracker%colors.length;  //To keep track of colors
-        let colorHeader = ticket.querySelector(".head-color");
-        let curColor = colors[tracker];
-        let prevColor = colorHeader.classList[1];
-        if (prevColor === curColor) {
-            tracker++;
-            curColor = colors[tracker];
+
+    let ticketCrudSpace = ticket.querySelector(".ticket-sub-container");
+    handleTicketCrud(collectiveTicketContainer, ticket, ticketCrudSpace, id);
+    let ticketLock = ticket.querySelector(".fas");
+    handleTicketLock(ticket, ticketLock);
+}
+
+function handleTicketLock(ticket, ticketLock) {
+    ticketLock.addEventListener("click", function() {
+        let ticketTaskEditor = ticket.querySelector(".task-desc");
+        let lockState = ticketLock.classList[1];
+        ticketLock.classList.remove(lockState);
+        if (lockState === "fa-lock") {
+            ticketLock.classList.add(unlockClass);
+            ticketTaskEditor.setAttribute("contenteditable", true);
         }
-        colorHeader.classList.remove(prevColor);
-        colorHeader.classList.add(curColor);
-        tracker++;
+        else {
+            ticketLock.classList.add(lockClass);
+            ticketTaskEditor.setAttribute("contenteditable", false);
+
+        }
+    });
+}
+
+function handleTicketCrud(collectiveTicketContainer, ticket, ticketCrudSpace, id) {
+
+    ticketCrudSpace.addEventListener("click", function () {
+        let ticketIdx;
+        ticketsArr.forEach((ele, idx) => {
+            if (ele.id === id) {
+                ticketIdx = idx;
+            }
+        }, id);
+
+        if (removeFlag == true) {  //Delete ticket
+            ticketsArr.splice(ticketIdx, 1);
+
+            let strTicketsArr = JSON.stringify(ticketsArr);
+            localStorage.setItem("availableTickets", strTicketsArr);
+            collectiveTicketContainer.removeChild(ticket);
+        }
+        else {  //Manage priority colors of ticket
+            let colorHeader = ticket.querySelector(".head-color");
+            let prevColor = colorHeader.classList[1];
+            let curColor = colors[(colors.indexOf(prevColor) + 1) % colors.length];
+            colorHeader.classList.remove(prevColor);
+            colorHeader.classList.add(curColor);
+            ticketsArr[ticketIdx].color = curColor;
+            let strArr = JSON.stringify(ticketsArr);
+            localStorage.setItem("availableTickets", strArr);
+        }
+
+        ticketCrudSpace.addEventListener("keydown", function (e) {    //Updating task content
+            if (e.ctrlKey && e.key == "Enter") {            //Use ctrl + enter to update edited task
+                let taskDesc = ticket.querySelector(".task-desc");
+                let updatedTask = taskDesc.innerText;
+                ticketsArr[ticketIdx].task = updatedTask;
+                let strArr = JSON.stringify(ticketsArr);
+                localStorage.setItem("availableTickets", strArr);
+            }
+        });
+
     });
 }
 
 function setToDefault() {
     modalContainer.style.display = "none";
     taskArea.value = "";
-    cColor = colors[colors.length-1];
-    priorityColor.forEach(function(colorElem) {
+    cColor = colors[colors.length - 1];
+    priorityColor.forEach(function (colorElem) {
         colorElem.classList.remove("border");
     });
-    priorityColor[priorityColor.length-1].classList.add("border");
+    priorityColor[priorityColor.length - 1].classList.add("border");
 }
-
-
-
-
-
-
-// let addModal = document.querySelector(".add");
-
-// addModal.addEventListener("click", function() {
-//     console.log('clicked');
-//     // modal container - create & set attribute
-//     let modalContainer = document.createElement("div");
-//     modalContainer.classList.add("modal-container");
-
-//     // task container - create & set attribute
-//     let taskContainer = document.createElement("div");
-//     taskContainer.classList.add("task-container");
-//     let textArea = document.createElement("textarea");
-//     textArea.setAttribute("placeholder", "Enter your task here");
-//     taskContainer.appendChild(textArea);
-
-//     // priority container - create & set attribute
-//     let priorityContainer = document.createElement("div");
-//     priorityContainer.classList.add("priority-container");
-
-//     let priorityColorContainer = document.createElement("div")
-//     priorityColorContainer.classList.add("priority-color-container");
-    
-//     let priorityColor = document.createElement("div");
-//     priorityColor.classList.add("pink", "priority-color");
-//     priorityColorContainer.appendChild(priorityColor);
-    
-//     priorityColor = document.createElement("div");
-//     priorityColor.classList.add("lightblue", "priority-color");
-//     priorityColorContainer.appendChild(priorityColor);
-
-//     priorityColor = document.createElement("div");
-//     priorityColor.classList.add("lightgreen", "priority-color");
-//     priorityColorContainer.appendChild(priorityColor);
-
-//     priorityColor = document.createElement("div");
-//     priorityColor.classList.add("black", "priority-color");
-//     priorityColorContainer.appendChild(priorityColor);
-    
-//     priorityContainer.appendChild(priorityColorContainer);
-
-//     modalContainer.appendChild(taskContainer);
-//     modalContainer.appendChild(priorityContainer);
-
-//     //Add modal to main-container box
-//     mainContainerBox.appendChild(modalContainer);
-
-// });
--- 2_Browser/DOM_JIRA/raw/todo/script.js
+++ 2_Browser/DOM_JIRA/raw/todo/script.js
@@ -1,5 +1,30 @@
 let inputBox = document.querySelector(".input-box");
 let taskList = document.querySelector(".task-list");
+let tasksArr = [];
+
+if (localStorage.getItem("allTasks")) {
+    let strArr = localStorage.getItem("allTasks");
+    tasksArr = JSON.parse(strArr);
+    for (let i = 0;i < tasksArr.length;i++) {
+        let task = tasksArr[i];
+        let taskElem = document.createElement("li");  //create an element of li tag
+        taskElem.setAttribute("class", "task");
+        taskElem.innerText = task;
+        taskList.appendChild(taskElem);
+
+        inputBox.value = "";
+
+        taskElem.addEventListener("dblclick", function() {
+            let elem = taskElem.innerText;
+            let elemIdx = tasksArr.indexOf(elem);
+            tasksArr.splice(elemIdx, 1);
+            let strArr = JSON.stringify(tasksArr);
+            localStorage.setItem("allTasks", strArr);
+
+            taskElem.remove(); //remove this element on double click event
+        });
+    }
+}
 
 console.log("Before");
 
@@ -13,13 +38,23 @@ inputBox.addEventListener("keypress", function(e) {   // e in function -> is the
         let taskElem = document.createElement("li");  //create an element of li tag
         taskElem.setAttribute("class", "task");
         taskElem.innerText = task;
+        tasksArr.push(task);
+        let strArr = JSON.stringify(tasksArr);
+        localStorage.setItem("allTasks", strArr);
+
         taskList.appendChild(taskElem);  //Add a element in a selector
 
         inputBox.value = ""; //Empty the input value after task is added
 
         taskElem.addEventListener("dblclick", function() {
+            let elem = taskElem.innerText;
+            let elemIdx = tasksArr.indexOf(elem);
+            tasksArr.splice(elemIdx, 1);
+            let strArr = JSON.stringify(tasksArr);
+            localStorage.setItem("allTasks", strArr);
+
             taskElem.remove(); //remove this element on double click event
-        })
+        });
     }
 });
 
