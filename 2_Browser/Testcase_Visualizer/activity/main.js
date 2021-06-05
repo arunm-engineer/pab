f76f3861217cb2cce45e0aa76e538a51e6c5de0f
Arun <arun8778451315@gmail.com>
2021-06-06 02:21:26 +0530

    Modification in removal

--- 2_Browser/Testcase_Visualizer/activity/main.js
+++ 2_Browser/Testcase_Visualizer/activity/main.js
@@ -4,7 +4,7 @@ let levelIndicator = document.querySelector(".tree-level-indicator-btn")
 let removeSubtreeBtn = document.querySelector(".remove-subtree-btn");
 let highlightBtn = document.querySelector(".highlighter-btn");
 
-let removeSubtreeFlag = false;
+let removeSubtreeFlag = false;                              // Flag for removal of subtree accr to removeSubTreeBtn
 
 let activeColor = "#fff200";
 let inactiveColor = "#f5f6fa";
@@ -15,7 +15,7 @@ decrBtn.addEventListener("click", (e) => {
     if (level > -1) {
         level--;
         levelIndicator.textContent = level;
-        clearTree();
+        clearTree();                                          // Clear existing tree node and node connectors to draw tree with decr level 
         displayTree(level);
     }
 });
@@ -24,7 +24,7 @@ incrBtn.addEventListener("click", (e) => {
     if (level < 6) {
         level++;
         levelIndicator.textContent = level;
-        clearTree();
+        clearTree();                                         // Clear existing tree node and node connectors to draw tree with incr level 
         displayTree(level);
     }
 });
@@ -44,43 +44,41 @@ removeSubtreeBtn.addEventListener("click", (e) => {
 
 function removeSubtree(e) {
     if (removeSubtreeFlag) {
-        let subtreeNode = e.target;
+        let subtreeNode = e.target;                                                     // Target node
         let subTreeNodeID = subtreeNode.getAttribute("id");
         let subtreeContainer = subtreeNode.parentElement;
 
         let childtreeContainer = subtreeContainer.querySelectorAll(".node-cont");
         for (let i = 0;i < childtreeContainer.length;i++) {
-            childtreeContainer[i].remove();
+            childtreeContainer[i].remove();                                             // Remove children nodes container
         }
+
+        let canvas = document.querySelector("canvas");                                  // Get canvas for future use (clear & redraw connectors)
+        let tool = canvas.getContext("2d");
 
         let rootContainer = document.querySelector(".root-cont");
         let rootRectObj = rootContainer.getBoundingClientRect();
 
-        let canvas = document.querySelector("canvas");
-        let tool = canvas.getContext("2d");
-
-        let top = rootRectObj.top - (3*16) - (3*16);                        // header 3rem, controls 3rem
-        tool.clearRect(rootRectObj.left, top, rootRectObj.width, rootRectObj.height);
+        let top = rootRectObj.top - (3*16) - (3*16);                                    // header 3rem, controls 3rem
+        tool.clearRect(rootRectObj.left, top, rootRectObj.width, rootRectObj.height);  // Clear canvas to remove connectors
 
         let treeAvailableNodes = rootContainer.querySelectorAll(".node");
-        drawAvailableNodeConnectors(treeAvailableNodes, tool, subTreeNodeID); // Draw connector P -> L,R child nodes
+        drawAvailableNodeConnectors(treeAvailableNodes, tool, subTreeNodeID);     
     }
 }
 
-function drawAvailableNodeConnectors(treeAvailableNodes, tool, subTreeNodeID) {
+function drawAvailableNodeConnectors(treeAvailableNodes, tool, subTreeNodeID) {        // Redraw connector for available nodes (P -> L,R child nodes)
 
     Array.from(treeAvailableNodes).forEach( node => {
         let nodeID = node.getAttribute("id");
         if (subTreeNodeID != nodeID) {
             let nodeInfo = nodeObj[nodeID];
-            console.log(nodeID);
-            console.log(nodeInfo);
     
             if (nodeInfo) {
                 tool.lineWidth = 1;
-                tool.strokeStyle = "#227093";                             // Page BGcolor for accurate transparency of child connector nodes
+                tool.strokeStyle = "#227093";                                       // Page BGcolor for accurate transparency of child connector nodes
     
-                tool.beginPath();                                         // Erase P -> L,R child node connectors
+                tool.beginPath();                                                    // Erase P -> L,R child node connectors
                 tool.moveTo(nodeInfo.PCenX, nodeInfo.PCenY);
                 tool.lineTo(nodeInfo.LCenX, nodeInfo.LCenY);
                 tool.stroke();
@@ -96,3 +94,9 @@ function drawAvailableNodeConnectors(treeAvailableNodes, tool, subTreeNodeID) {
     });
 }
 
+function getChildCount(nodeID) {
+    let node = document.querySelector(`.node[id="${nodeID}"]`);
+    let parentContainer = node.parentElement;
+    let childContainer = parentContainer.querySelectorAll(".node-cont");
+    return childContainer.length;
+}
